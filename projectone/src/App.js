b65c4bce4e0830af009f2f72317e295490bbd85a
mohamekni <mekni.moha99@gmail.com>
2025-09-15 00:23:16 +0200

    ProjectOne

--- projectone/src/App.js
+++ projectone/src/App.js
@@ -2,7 +2,13 @@ import {data} from './data';
 import Card from './Components/Card';
 
 export default function App() {
-const dataShow = data.map(item => <Card img={item.img} title={item.title} review={item.review} price={item.price} /> )
+const dataShow = data.map(item => <Card 
+  img={item.img} 
+  title={item.title} 
+  review={item.review} 
+  price={item.price} 
+  /> 
+)
   return (
     <div>
       {dataShow}
--- projectone/src/Components/Card.js
+++ projectone/src/Components/Card.js
@@ -1,6 +1,10 @@
 export default function Card(props) {
-return <div>
-        <img src={props.img} alt="logo"/>
-        <h1>{props.title}</h1>
+return <div style={{width:"250px"}}>
+        <img src={props.img} style={{width:"250px", height:"300px"}} alt="logo"/>
+        <div style={{display:"flex",alignItems:"center", justifyContent:"space-between"}}>
+            <h1 style={{}}>{props.title}</h1>
+            <h3>{props.review}</h3>
+        </div>
+        <h3 style={{marginTop:"0"}}>{props.price}$ night</h3>
     </div>
 }
