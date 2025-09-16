830acde871ff41f008da5492886a57817a9f1f08
mohamekni <mekni.moha99@gmail.com>
2025-09-16 17:27:04 +0200

    useState, Forms

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 export default function App() {
-
-    const [x,setX] = React.useState(true);
-   function Toggle(){
-    setX((prev)=>!prev)
-   }
+    const [firstName, setFirstName] = React.useState('');
+    const [lastName, setLastName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+        console.log(firstName)
     return (
-        <div className="App">
-            <div style={{textAlign:"center"}} 
-                onClick={Toggle}>
-                    <i style={{color:x ? "red":"gold"}} className="fa-solid fa-star"></i>
-            </div>
+        <div>
+            <form action="">
+                <label htmlFor="1">First Name : </label>
+                <input type="text" id="1" placeholder="First Name" 
+                value={firstName} 
+                onChange={(e)=>setFirstName(e.target.value)} required/>
+
+                <label htmlFor="2"> Last Name : </label>
+                <input type="text" id="2" placeholder="First Name" 
+                value={lastName} 
+                onChange={(e)=>setLastName(e.target.value)} required/>
+
+                <label htmlFor="3"> Email : </label>
+                <input type="text" id="3" placeholder="Email" 
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                required/>
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     ) ;
 }
