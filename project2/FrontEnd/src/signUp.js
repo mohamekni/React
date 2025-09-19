aa46ac29e3d1983791d39c7568467786b6391476
mohamekni <mekni.moha99@gmail.com>
2025-09-19 13:38:14 +0200

    SignUp

--- project2/FrontEnd/src/signUp.js
+++ project2/FrontEnd/src/signUp.js
@@ -1,4 +1,12 @@
-export default function signUp(){
+import { useState } from "react";
+
+export default function SignUp(){
+
+    const [name , setName] = useState("");
+    const [email , setEmail] = useState("");
+    const [password , setPass] = useState("");
+    const [passwordR , setPassR] = useState("");
+    console.log(name)
 
     function Submit(event){
         event.preventDefault();
@@ -9,13 +17,13 @@ export default function signUp(){
             <div className="register">
             <form onSubmit={Submit}>
                 <label htmlFor="1">UserName : </label>
-                <input type="text" placeholder="username" id="1" required/>
+                <input type="text" placeholder="username" id="1" value={name} onChange={(e)=> setName(e.target.value)}/>
                 <label htmlFor="2">Email : </label>
-                <input type="email" placeholder="Email" id="2" required/>
+                <input type="email" placeholder="Email" id="2" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
                 <label htmlFor="3">Password : </label>
-                <input type="Password" placeholder="Password" id="3" required/>
+                <input type="Password" placeholder="Password" id="3" value={password} onChange={(e)=> setPass(e.target.value)} />
                 <label htmlFor="4">Repeat Password : </label>
-                <input type="Password" placeholder="Password" id="4" required/>
+                <input type="Password" placeholder="Password" id="4" value={passwordR} onChange={(e)=> setPassR(e.target.value)} />
                 <div style={{textAlign:"center"}}>
                     <button type="submit">Register</button>
                 </div>
