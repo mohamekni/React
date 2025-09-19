c72a23887c89ce1c5bca230f548b2427e93f62ac
mohamekni <mekni.moha99@gmail.com>
2025-09-19 15:05:32 +0200

    SignUp

--- project2/FrontEnd/src/signUp.js
+++ project2/FrontEnd/src/signUp.js
@@ -6,10 +6,13 @@ export default function SignUp(){
     const [email , setEmail] = useState("");
     const [password , setPass] = useState("");
     const [passwordR , setPassR] = useState("");
+    const [error , setError] = useState(false);
+
     console.log(name)
 
     function Submit(event){
         event.preventDefault();
+        setError(true);
     }
 
     return (
@@ -18,12 +21,18 @@ export default function SignUp(){
             <form onSubmit={Submit}>
                 <label htmlFor="1">UserName : </label>
                 <input type="text" placeholder="username" id="1" value={name} onChange={(e)=> setName(e.target.value)}/>
+
                 <label htmlFor="2">Email : </label>
                 <input type="email" placeholder="Email" id="2" value={email} onChange={(e)=> setEmail(e.target.value)} required/>
+
                 <label htmlFor="3">Password : </label>
                 <input type="Password" placeholder="Password" id="3" value={password} onChange={(e)=> setPass(e.target.value)} />
+
+                {password.length < 8 && error && <p style={{color:"red"}}>Password must be 8 characters long</p> }
+
                 <label htmlFor="4">Repeat Password : </label>
                 <input type="Password" placeholder="Password" id="4" value={passwordR} onChange={(e)=> setPassR(e.target.value)} />
+                {passwordR !== password && error && <p style={{color:"red"}}>Passwords does not match</p> }
                 <div style={{textAlign:"center"}}>
                     <button type="submit">Register</button>
                 </div>
